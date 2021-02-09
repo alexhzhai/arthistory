bb4c44e6930ba299847a275f2fe88e3b72bbb850
Alex Zhai <alexhezhai@gmail.com>
2021-02-08 21:08:11 -0800

    that was annoying

--- points.js
+++ points.js
@@ -1,5 +1,3 @@
 var points = [
-[1,Apollo 11 stones,Namibia,"-22.7297408,15.3922063","c. 25,500-25,300 BCE",Charcoal on stone,images/apollo11stones.jpg,"],"
-
-[2,White Temple and its ziggurat,"Uruk (modern Warka, Iraq)","31.3259573,45.6360812",c. 3500-3000 BCE,Mud brick,,"],"
+    [1,"Apollo 11 stones","Namibia",-22.7297408, 15.3922063,"c. 25,500-25,300 BCE","Charcoal on stone","apollo11stones.jpg"]
 ]
