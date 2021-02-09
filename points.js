3cbbffb9adfdb45dc9167af0b800ff6d385264d8
Alex Zhai <alexhezhai@gmail.com>
2021-02-09 00:09:12 -0800

    fix rome

--- points.js
+++ points.js
@@ -3,6 +3,6 @@ var points = [
     [2,"White Temple and its ziggurat",,"Uruk (modern Warka, Iraq)",31.3259573,45.6360812,"c. 3500-3000 BCE","Mud brick","-"],
     [1,"Great Hall of the Bulls",,"Lascaux, France",45.0539181,1.1588962,"Paleolithic Europe; 15,000-13,000 BCE","Rock painting","-"],
     [1,"Camelid sacrum in the shape of a canine",,"Tequixquiac, central Mexico",19.9039462,-99.1588837,"14,000-7000 BCE","Bone","-"],
-    [3,"Catacomb of Priscilla",,"Rome, Italy",41.8483742,11.3909678,"Late Antique Europe. c. 200-400 CE","Excavated tufa and fresco","-"],
+    [3,"Catacomb of Priscilla",,"Rome, Italy",41.8905084, 12.4902419,"Late Antique Europe. c. 200-400 CE","Excavated tufa and fresco","-"],
     [4,"Portrait of Sor Juana Inés de la Cruz","Miguel Cabrera",,19.3970183,-99.2120457,"c. 1750","Oil on canvas","-"]
 ]
