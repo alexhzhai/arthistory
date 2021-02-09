3c154f3fa6ecdd56d194b64c408c9f3ecb352ddb
Alex Zhai <alexhezhai@gmail.com>
2021-02-08 23:27:29 -0800

    checkboxes

--- points.js
+++ points.js
@@ -1,5 +1,7 @@
 var points = [
-    [1,"Apollo 11 stones","Namibia",-22.7297408, 15.3922063,"c. 25,500-25,300 BCE","Charcoal on stone","apollo11stones.jpg"],
-    [2,"White Temple and its ziggurat","Uruk (modern Warka, Iraq)", 31.3259573,45.6360812,"c. 3500-3000 BCE","Mud brick","-"]
+    [1,"Apollo 11 stones","Namibia",-22.7297408,15.3922063,"c. 25,500-25,300 BCE","Charcoal on stone","apollo11stones.jpg"],
+    [2,"White Temple and its ziggurat","Uruk (modern Warka, Iraq)",31.3259573,45.6360812,"c. 3500-3000 BCE","Mud brick","-"],
+    [1,"Great Hall of the Bulls","Lascaux, France",45.0539181,1.1588962,"Paleolithic Europe; 15,000-13,000 BCE","Rock painting","-"],
+    [1,"Camelid sacrum in the shape of a canine","Tequixquiac, central Mexico",19.9039462,-99.1588837,"14,000-7000 BCE","Bone","-"]
 
 ]
